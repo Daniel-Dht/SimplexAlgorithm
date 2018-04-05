c013c143cb07fa5005a367bfbfa8de38a479d299
Daniel-Dht <daniel.douheret@ecl16.ec-lyon.fr>
2018-04-05 11:11:39 +0200

    simplex, with expansion/compression/reduction

--- simplex.py
+++ simplex.py
@@ -8,18 +8,18 @@ Created on Mon Mar 27 10:36:14 2017
 from tkinter import *
 import time
 
-WIDTH = 800
-HEIGHT = 500
+WIDTH = 600
+HEIGHT = 600
 SIZE = 50
 tk = Tk()
 canvas = Canvas(tk, width=WIDTH, height=HEIGHT, bg="grey")
 canvas.pack()
-color = 'black'
 
-xmin = -5
-xmax =  5
-ymin = -5
-ymax =  5
+fen = 6
+xmin = -fen
+xmax =  fen
+ymin = -fen
+ymax =  fen
 
     
 def xPixel2Domain(x):
@@ -31,11 +31,13 @@ def yPixel2Domain(y):
 def f(x,y):
     x = xPixel2Domain(x)
     y = yPixel2Domain(y)
-    return x**2+y**2
+    #return x**2+y**2
+    return (x**2 + y - 11)**2 + (x + y**2 - 7)**2 #  Himmelblau’s function 
+
   
 class Simplex:
     def __init__(self):
-        self.points = [[150,200,0],[200,150,0],[300,200,0]] 
+        self.points = [[150,180,0],[170,180,0],[160,210,0]] 
         self.line1 = canvas.create_line(self.points[0][0], self.points[0][1], self.points[1][0], self.points[1][1])
         self.line2 = canvas.create_line(self.points[0][0], self.points[0][1], self.points[2][0], self.points[2][1])
         self.line3 = canvas.create_line(self.points[1][0], self.points[1][1], self.points[2][0], self.points[2][1])
@@ -48,9 +50,13 @@ class Simplex:
         bestValue = self.points[0][2]
         bestIndex = 0
         for k in range(3):
-            if self.points[k][2] > bestValue :
+            if self.points[k][2] < bestValue :
                 bestValue = self.points[k][2] 
                 bestIndex = k
+        print("meilleur valeur ", bestValue," pos : (",self.points[bestIndex][0],self.points[bestIndex][0],")"  )
+        x1 = self.points[bestIndex][0]-5#for debug
+        x2 = self.points[bestIndex][0]+5#for debug
+        canvas.create_oval( x1,  self.points[bestIndex][1]-5 , x2, self.points[bestIndex][1]+5, fill="green") #for debug
         return bestIndex
 
     def findWorst(self):
@@ -61,10 +67,10 @@ class Simplex:
                 worstValue = self.points[k][2] 
                 worstIndex = k
         self.indexTab.remove(worstIndex)        
-        x1 = self.points[worstIndex][0]-10
-        x2 = self.points[worstIndex][0]+10
-        canvas.create_oval( x1,  self.points[worstIndex][1]-10 , x2, self.points[worstIndex][1]+10) #for debug
-        print("plus grande valeur ", worstValue)
+        x1 = self.points[worstIndex][0]-5#for debug
+        x2 = self.points[worstIndex][0]+5#for debug
+        canvas.create_oval( x1,  self.points[worstIndex][1]-5 , x2, self.points[worstIndex][1]+5) #for debug
+        #print("plus faible valeur ", worstValue)
         return worstIndex
         
     def moveWorstPoint(self,id,bestID): # On cherche le barycentre (x,y) des points restant
@@ -73,6 +79,9 @@ class Simplex:
         valB = self.points[self.indexTab[1]][2]
         x = ( valA*self.points[self.indexTab[1]][0] + valB*self.points[self.indexTab[0]][0] ) / (valA+valB)
         y = ( valA*self.points[self.indexTab[1]][1] + valB*self.points[self.indexTab[0]][1] ) / (valA+valB)
+        
+        canvas.create_oval( x-5,  y-5 , x+5,y+5, fill="black") #for debug
+        
         newx = 2*x - self.points[id][0]     
         newy = 2*y - self.points[id][1]
         newValue = f(newx,newy)
@@ -80,6 +89,7 @@ class Simplex:
         finaly = newy
         # on regarde si on va vers la bonne direction == EXPANSION
         if newValue < self.points[self.indexTab[0]][2] and newValue < self.points[self.indexTab[1]][2]:
+            print("expansion  !")
             newx = 2*newx - x
             newy = 2*newy - y
             finalx = newx
@@ -87,34 +97,36 @@ class Simplex:
         # on check pour contraction si la nouvelle valeurs est la pluse mauvaise des 3 = CONTRACTION
         elif newValue > self.points[self.indexTab[0]][2] and newValue > self.points[self.indexTab[1]][2]:
             # si elle est meilleur que l'ancienne :
-            if newValue < self.points[id][2]:
-                contractx = 0.5*newx - 0.5 * x
-                contracty = 0.5*newy - 0.5 * y
+            if newValue < self.points[id][2] :
+                print("contraction avec le nouveau point !")
+                contractx = 0.5*newx + 0.5 * x
+                contracty = 0.5*newy + 0.5 * y
             else :
-                contractx = 0.5*self.points[id][0] - 0.5 * x
-                contracty = 0.5*self.points[id][1] - 0.5 * y 
+                print("contraction avec l'ancien point !")
+                contractx = 0.5*self.points[id][0] + 0.5 * x
+                contracty = 0.5*self.points[id][1] + 0.5 * y 
+                canvas.create_line( self.points[id][0],self.points[id][1] , x,y, fill="green") #for debug
+                
             finalx = contractx
             finaly = contracty
             # On recalcule la valeur du nouveau point
             contractValue = f(contractx,contracty)
             # Si il est toujours le plus mauvais, on réduit le triangle sur le meilleur point = REDUCTION
-            if contractValue > self.points[id][2]:
+            if contractValue > self.points[id][2] :
                 reduction = True
-                # Reduction avec les 3 anciennes valeurs si la nouvelle est plus mauvaise
-                if newValue > self.points[id][2]:
-                    for k in range(3):
-                        self.points[k][0] = 0.5*(self.points[k][0] + self.points[bestID][0])
-                        self.points[k][1] = 0.5*(self.points[k][1] + self.points[bestID][1])
-                # Reduction avec les 2 anciennes valeurs et la nouvelle
-                else :
-                    # il faut réassigner le pount qui a bougé
+                print("reduction !")
+                # Avant de réduire, il faut réassigner le pount qui a bougé si la nouvelle valeurs est meilleur que l'ancienne
+                if newValue < self.points[id][2]:
                     self.points[id][0] = newx
                     self.points[id][1] = newy 
-                    for k in range(3): # puis on modifis les valeurs
-                        self.points[k][0] = 0.5*(self.points[k][0] + self.points[bestID][0])
-                        self.points[k][1] = 0.5*(self.points[k][1] + self.points[bestID][1])
-                    
-
+                  
+                # Puis on réduit, et recalcules les valeurs en chaque point
+                for k in range(3):
+                    self.points[k][0] = 0.5*(self.points[k][0] + self.points[bestID][0])
+                    self.points[k][1] = 0.5*(self.points[k][1] + self.points[bestID][1])
+                    self.points[id][2] = f( self.points[0][0] ,  self.points[0][1] )
+                
+                
                     
         # on réinitialise la liste d'index, les coordonnées du point et sa valeur
         self.indexTab = [0,1,2] 
@@ -131,32 +143,41 @@ class Simplex:
     
         
 simplex = Simplex()
-i = 0 
-while( i<20 ):
-    i+=1
+# i = 0 
+# while( i<7 ):
+#     i+=1
+#     print("************  ",i,"   *****************")
+#     wID = simplex.findWorst()
+#     bID = simplex.findBest()
+#     simplex.moveWorstPoint(wID, bID)
+
+def key(event):
+    canvas.focus_set()
+    print( "pressed", repr(event.char))
+    
+def callback(event):
+    canvas.focus_set()
+    print( "clicked at", event.x, event.y)
     wID = simplex.findWorst()
     bID = simplex.findBest()
     simplex.moveWorstPoint(wID, bID)
 
+
+canvas.bind("<Key>", key)
+canvas.bind("<Button-1>", callback)
+canvas.pack()
+
 tk.mainloop()
 
+
+
 ##
-    
-      if(x > a[0]) {
-        a[0] += 2*abs(a[0]-x); 
-      } else {
-        a[0] -= 2*abs(a[0]-x); 
-      }
-      if(y > a[1]) {
-        a[1] += 2*abs(a[1]-y); 
-      } else {
-        a[1] -= 2*abs(a[1]-y); 
-      }      
-    
+can = Canvas(root, width=100, height=100, bg="grey")
+can.pack()
 
-
-
-
+can.bind("<Key>", key)
+can.bind("<Button-1>", callback)
+can.pack()
 
 
 
